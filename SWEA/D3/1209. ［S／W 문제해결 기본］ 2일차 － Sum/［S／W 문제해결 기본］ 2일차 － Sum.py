45e44395b8f8d187ff24890e08ebad512bf7c756
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2022-08-10 13:09:53 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 225 ms, Memory: 63,036 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -2,6 +2,7 @@
 # 2022-08-10
 
 
+# 최대값 구하는 함수를 만든다.
 def maxV(arr):
     maxV = 0
     for i in arr:
@@ -9,14 +10,18 @@ def maxV(arr):
             maxV = i
     return maxV
 
+
 for _ in range(1, 11):
 
+    # 테스트케이스 번호를 입력받는다.
     tc = int(input())
+    # 2차원 배열의 각 행 값을 입력받아 리스트 형태로 arr리스트에 저장한다. (2차원 리스트 생성)
     arr = [list(map(int,input().split())) for _ in range(100)]
 
+    # 각 행, 열, 대각선의 합들을 넣을 ans 리스트를 생성한다.
     ans = []
 
-    # 행 우선 순회
+    # 행 우선 순회를 하면서 각 행의 합을 구한다.
     for i in range(100):
         total = 0
         for j in range(100):
@@ -24,7 +29,7 @@ for _ in range(1, 11):
         ans.append(total)
         
     
-    # 열 우선 순회
+    # 열 우선 순회를 하면서 각 열의 합을 구한다.
     for i in range(100):
         total = 0
         for j in range(100):
@@ -37,4 +42,10 @@ for _ in range(1, 11):
         total += arr[i][i]
     ans.append(total)
 
+    total = 0
+    for i in range(10):
+        total += arr[-i][-i]
+    ans.append(total)
+
+    # 정답을 출력한다.
     print('#{} {}'.format(tc, maxV(ans)))
