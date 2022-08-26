34397efd4236892c15d3b1a60172f34c897828b5
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2022-08-26 11:44:22 +0900

    [Silver II] Title: 섬의 개수, Time: 140 ms, Memory: 32484 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -4,12 +4,15 @@ from collections import deque
 
 def bfs(x, y):
     global w, h, cnt
+    # 큐를 생성하고 시작지점 추가
     queue = deque()
     queue.append((x, y))
+    # 땅(1)의 좌표만 검사할 것이므로 cnt에 1 더해주기
     cnt += 1
+    # 중복을 방지하기 위해 추가한 땅은 -1로 바꿔준다.
     map_list[x][y] = -1
 
-
+    # 큐가 빌 때까지 반복
     while queue:
         x, y = queue.popleft()
         # 현재 위치에서 8가지 방향으로 땅(1) 탐색
@@ -19,8 +22,10 @@ def bfs(x, y):
             # 영역을 벗어난 경우 무시
             if nx < 0 or nx >= h or ny < 0 or ny >= w:
                 continue
+            # 바다(0)인 경우 무시
             elif map_list[nx][ny] == 0:
                 continue
+            # 땅(1)인 경우, -1로 바꿔주고 좌표를 큐에 추가
             elif map_list[nx][ny] == 1:
                 map_list[nx][ny] = -1
                 queue.append((nx, ny))
@@ -31,7 +36,7 @@ while True:
 
     if w == 0 and h == 0:
         break
-    
+
     map_list = []
     for _ in range(h):
         map_list.append(list(map(int, input().split())))
