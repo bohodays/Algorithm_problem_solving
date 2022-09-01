ddcc75a25441240835d1f71b73d14a39c85373b8
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2022-09-01 23:58:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 1504 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,13 @@
+kg = int(input())
+
+cnt = 0
+for i in range(1667):
+    if cnt == 1:
+        break
+    for j in range(5001):
+        if 3*i + 5*j == kg:
+            cnt = 1
+            print(i + j)
+            break
+if cnt == 0:
+    print(-1)
