76e7c37d64fbd9ab1c244cd169de4a0e1d63ab59
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2022-09-25 20:50:19 +0900

    [D4] Title: 정사각형 방, Time: 2,020 ms, Memory: 186,868 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,42 +1,38 @@
-# 1861 정사각형 방
-# 2022-09-13
+# 정사각형 방
 
-from collections import deque
+def DFS(x, y):
+    global cnt, ans
+    cnt += 1
 
-# 이동할 4가지 방향 설정(상하좌우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            if arr[x][y] + 1 == arr[nx][ny]:
+                DFS(nx, ny)
 
-def bfs(x, y):
-    cnt = 0
-    queue = deque()
-    queue.append((x, y))
-
-    while queue:
-        x, y = queue.popleft()
-        cnt += 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if graph[x][y] + 1 == graph[nx][ny]:
-                    queue.append((nx, ny))
-    return cnt
 T = int(input())
 
 for tc in range(1, T+1):
 
     N = int(input())
 
-    graph = [list(map(int, input().split())) for _ in range(N)]
+    arr = []
+    for _ in range(N):
+        arr.append(list(map(int, input().split())))
 
-    ans = [graph[0][0], bfs(0,0)]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    ans = [float('inf'), 0]
     for i in range(N):
         for j in range(N):
-            tmp = bfs(i,j)
-            if tmp > ans[1]: 
-                ans[0] = graph[i][j]
-                ans[1] = tmp
-            elif tmp == ans[1]:
-                ans[0] = min(graph[i][j], ans[0])
+            cnt = 0
+            DFS(i, j)
+            if cnt > ans[1]:
+                ans[0] = arr[i][j]
+                ans[1] = cnt
+            elif cnt == ans[1]:
+                ans[0] = min(ans[0], arr[i][j])
+    
     print('#{}'.format(tc), *ans)
