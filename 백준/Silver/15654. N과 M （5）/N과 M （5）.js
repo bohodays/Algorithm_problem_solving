190525b6a96c9ed358d1044c07144b56e80e19a2
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-01-16 00:01:25 +0900

    [Silver III] Title: N과 M (5), Time: 252 ms, Memory: 21112 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,31 @@
+// 15654 N과 M (5)
+// N개의 자연수 중에서 M개를 고른 수열
+
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m] = inputData[0].split(' ').map((num) => +num);
+const numbers = inputData[1].split(' ').map((num) => +num).sort((a, b) => a - b);
+
+let ans = '';
+const result = [];
+const visited = Array(n).fill(false);
+
+const dfs = (count) => {
+  if (count === m) {
+    ans += result.join(' ') + '\n';
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      result[count] = numbers[i];
+      dfs(count + 1);
+      visited[i] = false;
+    }
+  }
+}
+
+dfs(0);
+console.log(ans);
