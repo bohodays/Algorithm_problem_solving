e2004889c02bedc8b22a6a4f888d92c349b230ba
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-03-27 02:04:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 10.22 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,10 @@
+function solution(arr1, arr2) {
+    const ans = Array(arr1.length).fill().map(() => Array(arr1[0].length).fill(0));
+
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr1[0].length; j++) {
+            ans[i][j] = arr1[i][j] + arr2[i][j];            
+        }
+    }
+    return ans;
+}
