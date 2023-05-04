659d34735019e3329b32d553d4e40fa0a1e91ad8
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-05-05 01:41:35 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int solution(int slice, int n) {
+
+    
+   return ((n - 1) / slice) + 1;
+}
