f79a98882f793f87d59e5c23e2648f5697ab2f26
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-05-11 00:19:14 +0900

    [unrated] Title: 옹알이 (2), Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -4,12 +4,16 @@ function solution(babbling) {
         let flag = true;
         const checkItem = babbling[i];
         for (const item of ['aya', 'ye', 'woo', 'ma']) {
+            // 두번 이상 반복한 것이 포함되면 같은 발음을 연속 발음한 것이므로
+            // 반복을 멈추고 넘어간다.
             if (checkItem.includes(item.repeat(2))) {
                 flag = false;
                 break;
             }
         }
+        // 두번 연속으로 발음하지 않았으면
         if (flag) {
+            // 발음할 수 있는 단어인지 확인
             if (checkItem.replace(/aya|ye|woo|ma/g, '') === '') {
                 ans++;
             }
