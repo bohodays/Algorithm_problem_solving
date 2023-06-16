7ef1ab1339da7315d6d8525cc15017b6dd27c35a
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-06-17 00:06:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    let answer = 0;
+    
+    while (a !== b) {
+        a = Math.round(a / 2);
+        b = Math.round(b / 2);
+        answer++;
+    }
+
+    return answer;
+}
