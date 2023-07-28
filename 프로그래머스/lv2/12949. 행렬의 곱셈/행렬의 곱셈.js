4caef0978f3004c683b86fdd698522a7db0bc7f7
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-07-28 09:01:21 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.12 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,17 @@
+function solution(arr1, arr2) {
+  const answer = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    const result = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let value = 0;
+      for (let k = 0; k < arr1[0].length; k++) {
+        value += arr1[i][k] * arr2[k][j];
+      }
+      result.push(value);
+    }
+    answer.push(result);
+  }
+
+  return answer;
+}
