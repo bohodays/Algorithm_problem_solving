9a97a7da45e6b3e9d0a3000c7b05a4be17550e0d
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-10-26 12:37:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.24 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+def solution(maps):
+    answer = 0
+    n = len(maps)
+    m = len(maps[0])
+    
+    visited = [[0 for i in range(m)] for j in range(n)]
+    
+    # 방향 설정 (상하좌우)
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    # 시작 지점 설정
+    visited[0][0] = 1
+    queue = deque()
+    queue.append([0, 0])
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if (0 <= nx < n) and (0 <= ny < m):
+                if not visited[nx][ny] and maps[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append([nx, ny])
+
+    return visited[n - 1][m - 1] if visited[n - 1][m - 1] else -1
+
+
+# from collections import deque
+
+# def solution(maps):
+#     answer = 0
+#     dx = [-1, 1, 0, 0]
+#     dy = [0, 0, -1, 1]
+    
+#     n, m = len(maps), len(maps[0])
+    
+#     queue = deque()
+#     queue.append([0, 0])
+    
+#     visited = [[0 for i in range(m)] for j in range(n)]
+#     visited[0][0] = 1
+    
+#     while len(queue):
+#         x, y = queue.popleft()
+        
+#         for i in range(4):
+#             nx, ny = x + dx[i], y + dy[i]
+#             if 0 <= nx < n and 0 <= ny < m:
+#                 if not visited[nx][ny] and maps[nx][ny] == 1:
+#                     queue.append([nx, ny])
+#                     visited[nx][ny] = visited[x][y] + 1
+                    
+#     return visited[n - 1][m - 1] if visited[n - 1][m - 1] else -1
+
