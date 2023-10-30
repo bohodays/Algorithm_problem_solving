dab8af419e5b509d932fc1dfa9f02370427f4e7e
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-10-30 22:18:12 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,22 @@
+from collections import deque
+
 def solution(progresses, speeds):
     answer = []
+    queue = deque(progresses)
     
-    while progresses:
-        # 각 작업의 진행 상태 업데이트
-        for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-        
+    while queue:
+        n = len(speeds)
         count = 0
-        # 가장 앞에 있는 작업이 100% 이상 완료된 경우 처리
-        while progresses and progresses[0] >= 100:
-            progresses.pop(0)
+        
+        for i in range(n):
+            queue[i] += speeds[i]
+        
+        while queue and queue[0] >= 100:
+            queue.popleft()
             speeds.pop(0)
             count += 1
-        
+            
         if count:
             answer.append(count)
-    
+            
     return answer
