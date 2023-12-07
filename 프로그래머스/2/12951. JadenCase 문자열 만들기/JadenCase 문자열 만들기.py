4e84f8b6a4146514d289cb9c393d529c74082eec
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-07 13:31:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,17 +1,6 @@
 def solution(s):
-    answer = []
-    s = s.split(" ")
-    
-    for item in s:
-        if len(item):
-            # 숫자가 아닌 경우
-            if not item[0].isdigit():
-                newItem = item[0].upper() + item[1:].lower()
-                answer.append(newItem)
-            else:
-            # 숫자인 경우
-                answer.append(item.lower())
-        else:
-            answer.append("")
-    print(answer)
-    return " ".join(answer)
+    test_list = s.split(" ")
+    result=[]
+    for i in test_list :
+        result.append(i.capitalize())
+    return ' '.join(result)
