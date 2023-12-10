6baf38d5ac109e5ae59acf3cd5f7344fad3b122a
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-10 11:47:11 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,35 @@
+def solution(brown, yellow):
+    answer = []
+    
+    for i in range(1, int(yellow**(0.5)) + 1):
+        width = yellow / i
+        height = i
+        
+        result = width * 2 + height * 2 + 4
+        if result == brown:
+            return [width + 2, height + 2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
