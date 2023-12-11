5ad6179e86dd5ed8ba08a51bc4bcf8c59443700b
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-11 22:41:12 +0900

    [level 2] Title: 귤 고르기, Time: 37.85 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 17.9 MB, 시간: 18.33 ms
+메모리: 17.9 MB, 시간: 37.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 15:36:36
+2023년 12월 1일 22:41:9
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,21 +1,19 @@
 def solution(k, tangerine):
     answer = 0
+    tangerine.sort()
     
     obj = {}
-    
     for item in tangerine:
         if item in obj:
             obj[item] += 1
         else:
             obj[item] = 1
     
-    value_list = sorted(obj.values(), reverse = True)
+    objToArray = sorted(obj.values())
     
-    for item in value_list:
+    for i in range(len(objToArray) - 1, -1, -1):
+        k -= objToArray[i]
         answer += 1
-        k -= item
         
         if k <= 0:
-            break
-    
-    return answer
+            return answer
