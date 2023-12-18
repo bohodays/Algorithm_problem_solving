20f176f144fc75f3d7fb5a559dec8f0c721ade5e
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-18 18:46:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,19 +2,15 @@ from collections import deque
 
 def solution(maps):
     answer = 0
-    n = len(maps)
-    m = len(maps[0])
+    queue = deque([(0, 0)])
+    n, m = len(maps), len(maps[0])
+    # 방문 여부 확인을 위한 배열 생성
+    visited = [[0] * m for _ in range(n)]
+    visited[0][0] = 1 # 출발점 방문 처리
     
-    visited = [[0 for i in range(m)] for j in range(n)]
-    
-    # 방향 설정 (상하좌우)
+    # 4방향 설정
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    
-    # 시작 지점 설정
-    visited[0][0] = 1
-    queue = deque()
-    queue.append([0, 0])
     
     while queue:
         x, y = queue.popleft()
@@ -22,39 +18,11 @@ def solution(maps):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
+            # 범위가 유효한지 확인
             if (0 <= nx < n) and (0 <= ny < m):
-                if not visited[nx][ny] and maps[nx][ny] == 1:
+                # 방문 여부 및 지나갈 수 있는 칸인지 확인
+                if not visited[nx][ny] and maps[nx][ny]:
                     visited[nx][ny] = visited[x][y] + 1
                     queue.append([nx, ny])
-
-    return visited[n - 1][m - 1] if visited[n - 1][m - 1] else -1
-
-
-# from collections import deque
-
-# def solution(maps):
-#     answer = 0
-#     dx = [-1, 1, 0, 0]
-#     dy = [0, 0, -1, 1]
-    
-#     n, m = len(maps), len(maps[0])
-    
-#     queue = deque()
-#     queue.append([0, 0])
-    
-#     visited = [[0 for i in range(m)] for j in range(n)]
-#     visited[0][0] = 1
-    
-#     while len(queue):
-#         x, y = queue.popleft()
-        
-#         for i in range(4):
-#             nx, ny = x + dx[i], y + dy[i]
-#             if 0 <= nx < n and 0 <= ny < m:
-#                 if not visited[nx][ny] and maps[nx][ny] == 1:
-#                     queue.append([nx, ny])
-#                     visited[nx][ny] = visited[x][y] + 1
                     
-#     return visited[n - 1][m - 1] if visited[n - 1][m - 1] else -1
-
+    return visited[n - 1][m - 1] if visited[n - 1][m - 1] else -1
