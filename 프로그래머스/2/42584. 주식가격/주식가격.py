daa7cbb07572b751350fc09cc1276538979a562b
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-20 13:16:13 +0900

    [level 2] Title: 주식가격, Time: 37.27 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,19 @@
 from collections import deque
 
 def solution(prices):
-    queue = deque(prices)
     answer = []
+    queue = deque(prices)
     
     while queue:
         current_price = queue.popleft()
         
         time = 0
-        for item in queue:
+        for price in queue:
             time += 1
-            if current_price > item:
+            if current_price > price:
                 break
         answer.append(time)
-        
+    
     return answer
+
+
