1e3a5ddbf75b3478477b64591b15c6eeb2bfee95
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-20 09:43:21 +0900

    [level 2] Title: 귤 고르기, Time: 71.09 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,19 +1,21 @@
 def solution(k, tangerine):
     answer = 0
-    tangerine.sort()
+    # 귤의 최대크기에 맞게 0으로 초기화한 배열 생성
+    num_array = [0] * (max(tangerine) + 1)
     
-    obj = {}
-    for item in tangerine:
-        if item in obj:
-            obj[item] += 1
-        else:
-            obj[item] = 1
+    # 인덱스에 맞게 귤의 개수 세기
+    for i in range(len(tangerine)):
+        num_array[tangerine[i]] += 1
     
-    objToArray = sorted(obj.values())
+    # 정렬하기
+    num_array.sort()
+    index = len(num_array) - 1
     
-    for i in range(len(objToArray) - 1, -1, -1):
-        k -= objToArray[i]
+    # k에서 같은 크기가 많은 순으로 빼면서 k가 0보다 작으면 answer 반환
+    while True:
+        k -= num_array[index]
         answer += 1
+        index -= 1
         
         if k <= 0:
             return answer
