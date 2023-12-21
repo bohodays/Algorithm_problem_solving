1515b3d96d4f72596ef7758d9b5c848a9ecd9622
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2023-12-21 13:25:46 +0900

    [level 2] Title: 오픈채팅방, Time: 79.68 ms, Memory: 42.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,31 @@
+def solution(record):
+    answer = []
+    id_matching_nickname = {}
+    messages = []
+    
+    for item in record:
+        item = item.split()
+        if len(item) == 3:
+            user_act, user_id, user_nickname = item
+        else:
+            user_act, user_id = item
+        
+        
+        
+        if user_act == "Enter":
+            message = "님이 들어왔습니다."
+            id_matching_nickname[user_id] = user_nickname
+        elif user_act == "Leave":
+            message = "님이 나갔습니다."
+        elif user_act == "Change":
+            message = "님이 들어왔습니다."
+            id_matching_nickname[user_id] = user_nickname 
+            
+        
+        if user_act != "Change":
+            messages.append([user_id, message])  
+    
+    for message in messages:
+        answer.append(id_matching_nickname[message[0]] + message[1])
+
+    return answer
