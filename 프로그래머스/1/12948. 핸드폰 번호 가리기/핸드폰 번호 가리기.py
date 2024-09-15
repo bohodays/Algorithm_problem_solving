7df7670f537ae4bb5c26fe55f3a8cc6c8ec1f245
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2024-09-15 12:07:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,2 +1,3 @@
 def solution(phone_number):
-    return "*" * (len(phone_number) - 4) + phone_number[len(phone_number) - 4:]
+    answer = '*' * (len(phone_number) - 4) + phone_number[-4:]
+    return answer
