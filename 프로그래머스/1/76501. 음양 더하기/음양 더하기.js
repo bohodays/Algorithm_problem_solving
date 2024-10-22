a8979898f81426e26f89eaff632429dd48247603
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2024-10-22 08:16:38 -0400

    [level 1] Title: 음양 더하기, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,4 @@
 function solution(absolutes, signs) {
-    let answer = 0;
-    
-    for (let i = 0; i < absolutes.length; i++) {
-        answer += (signs[i] ? absolutes[i] : -absolutes[i])
-    }
-    
-    return answer;
+    const answer = absolutes.map((item, index) => signs[index] ? item : item * -1);
+    return answer.reduce((sum, num) => sum + num, 0);
 }
