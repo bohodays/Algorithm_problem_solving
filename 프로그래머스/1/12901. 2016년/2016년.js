3297d4c302977f0fcfed769ea638a46e0a3eb341
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2024-10-27 07:26:35 -0400

    [level 1] Title: 2016년, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,10 +1,5 @@
 function solution(a, b) {
     const days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const weeks = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
-    const answer = weeks[(days.splice(0, a - 1).reduce((sum, num) => sum + num, 0) + b - 1) % 7]
-    return answer;
+    return weeks[(days.map((item, index) => index < (a - 1) ? item : 0).reduce((sum, num) => sum + num) + b - 1) % 7];
 }
-
-
-
-
