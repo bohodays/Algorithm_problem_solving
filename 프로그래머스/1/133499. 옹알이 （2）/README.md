30d884908871092fe610ba48c1df87cf36ac9661
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2024-10-28 13:13:27 -0400

    [level 1] Title: 옹알이 (2), Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.42 ms
+메모리: 33.6 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 29일 01:49:21
+2024년 10월 29일 02:13:25
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,23 +1,27 @@
+
+
 function solution(babbling) {
-    let ans = 0;
+    const words = ["aya", "ye", "woo", "ma"];
+    
+    let answer = 0;
     for (let i = 0; i < babbling.length; i++) {
         let flag = true;
-        const checkItem = babbling[i];
-        for (const item of ['aya', 'ye', 'woo', 'ma']) {
-            // 두번 이상 반복한 것이 포함되면 같은 발음을 연속 발음한 것이므로
-            // 반복을 멈추고 넘어간다.
-            if (checkItem.includes(item.repeat(2))) {
+        let targetItem = babbling[i];
+        for (const word of words) {
+            if (targetItem.includes(word.repeat(2))) {
                 flag = false;
                 break;
             }
         }
-        // 두번 연속으로 발음하지 않았으면
+        
         if (flag) {
-            // 발음할 수 있는 단어인지 확인
-            if (checkItem.replace(/aya|ye|woo|ma/g, '') === '') {
-                ans++;
-            }
+            words.forEach((word, index) => {
+                targetItem = targetItem.replaceAll(word, index);
+            })
+            if (!isNaN(targetItem)) answer++;
+
         }
     }
-    return ans;
+    
+    return answer;
 }
