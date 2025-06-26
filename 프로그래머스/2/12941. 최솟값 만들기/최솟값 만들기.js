e7b7701991524b245313ed4562f68a8e32b78f3d
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-06-26 19:41:23 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.86 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,6 +1,5 @@
 function solution(A,B){
     A = A.sort((a, b) => a - b);
     B = B.sort((a, b) => b - a);
-    const answer = A.map((item, index) => item * B[index]).reduce((sum, num) => sum + num);
-    return answer;
+    return A.map((_, i) => A[i] * B[i]).reduce((sum, num) => sum + num, 0);
 }
