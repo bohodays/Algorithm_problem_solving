21696dbc2d0c7868744db2b183cf1d07b0408baf
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-06-28 02:23:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.19 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,20 +1,22 @@
 function solution(s) {
-  let ans = [];
-  const testCase = s.split(" ");
-  for (const item of testCase) {
-    if (item) {
-      // 첫 문자가 알파벳인 경우
-      if (isNaN(item[0])) {
-        const addItem = item.toLowerCase().split("");
-        addItem[0] = addItem[0].toUpperCase();
-        ans.push(addItem.join(""));
-        // 첫 문자가 알파벳이 아닌 경우
-      } else {
-        ans.push(item.toLowerCase());
-      }
-    } else {
-      ans.push(item);
+    let answer = '';
+    
+    let temp = ""
+    for (const word of s) {
+        if (word !== " ") {
+            temp += word;
+        } else {
+            if (temp.length > 0) {
+                answer += temp[0].toUpperCase() + temp.slice(1).toLowerCase();
+                temp = "";
+            }
+            answer += word;
+        }
     }
-  }
-  return ans.join(" ");
+    
+    if (temp.length > 0) {
+        answer += temp[0].toUpperCase() + temp.slice(1).toLowerCase();
+    }
+    
+    return answer;
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.13 ms
+메모리: 33.2 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 24일 19:53:17
+2025년 06월 28일 02:23:08
 
 ### 문제 설명
 
