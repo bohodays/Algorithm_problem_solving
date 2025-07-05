e64ee570873201bd914f6a846849fc505be9073c
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-07-05 12:53:16 +0900

    [level 2] Title: 구명보트, Time: 32.24 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,22 +1,20 @@
 function solution(people, limit) {
-    var answer = 0;
-    people = people.sort((a, b) => a - b);
+    let answer = 0;
     
-    // 투포인터 이용
-    let [start, end] = [0, people.length - 1];
+    people.sort((a, b) => a - b);
+    
+    let start = 0;
+    let end = people.length - 1;
+    
     while (start <= end) {
-        const [minValue, maxValue] = [people[start], people[end]];
-        // 두 지점의 합이 limit보다 큰 경우
-        if (minValue + maxValue > limit) {
+        if (people[start] + people[end] > limit) {
             end--;
-        }
-        // 두 지점의 합이 limit보다 작거나 같은 경우
-        else {
+        } else {
             start++;
             end--;
         }
         answer++;
     }
-    
+
     return answer;
 }
