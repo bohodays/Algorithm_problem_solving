1841aef8bcda8a567f53ecb66e4c41e66b478e5a
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-07-09 21:10:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 3.57 ms
+메모리: 33.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 10일 22:44:40
+2025년 07월 09일 21:10:53
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,29 +1,29 @@
 function solution(n, words) {
     const answer = [];
-    const checkedWords = [];
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        if (!checkedWords.length) {
-            checkedWords.push(word);
-        } else {
-            // 3번 규칙 확인
-            const lastWord = checkedWords[checkedWords.length - 1];
-            console.log(word[0], lastWord[lastWord.length - 1])
-            if (word[0] !== lastWord[lastWord.length - 1]) {
-                return [(i + 1) % n || n, Math.ceil((i + 1) / n)];
-            }
-            // 4번 규칙 확인
-            else if (checkedWords.includes(word)) {
-                return [(i + 1) % n || n, Math.ceil((i + 1) / n)];
-            }
-            // 5번 규칙 확인
-            else if (word.length === 1) {
-                return [(i + 1) % n || n, Math.ceil((i + 1) / n)];
-            }
-            else {
-                checkedWords.push(word);
-            }
+    let count = 0;
+    
+    let prevWord = words[0];
+    const isExistWord = {
+        [prevWord]: true
+    };
+    for (let i = 1; i < words.length; i++) {
+        const targetWord = words[i];
+        count++;
+        
+        // 3번 규칙 확인
+        if (prevWord[prevWord.length - 1] !== targetWord[0]) {
+            return [(count % n) + 1, Math.ceil((count + 1) / n)];
         }
+        
+        // 4번 규칙 확인
+        if (isExistWord[targetWord]) {
+            return [(count % n) + 1, Math.ceil((count + 1) / n)];
+        }
+        
+        prevWord = targetWord;
+        isExistWord[targetWord] = true;
+        
     }
+        
     return [0, 0];
 }
