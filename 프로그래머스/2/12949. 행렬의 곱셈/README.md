20c50014f55b8b8d64393e9c217acee2b16c1079
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-07-17 22:15:38 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.52 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 38.2 MB, 시간: 37.58 ms
+메모리: 38.8 MB, 시간: 6.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 00:00:24
+2025년 07월 17일 22:15:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,18 +1,21 @@
 function solution(arr1, arr2) {
-    var answer = [];
+    const answer = [];
+    const n = arr1.length;
+    const m = arr2[0].length;
+    const l = arr2.length;
     
-    for (let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < n; i++) {
         const result = [];
-        for (let j = 0; j < arr2[0].length; j++) {
-            let sumValue = 0;
-            for (let k = 0; k < arr2.length; k++) {
-                sumValue += arr1[i][k] * arr2[k][j];
+        for (let j = 0; j < m; j++) {
+            let sum = 0
+            for (let k = 0; k < l; k++) {
+                sum += arr1[i][k] * arr2[k][j];
             }
-            result.push(sumValue);
+            result.push(sum);
         }
         answer.push(result);
     }
+    
+    
     return answer;
 }
-
-
