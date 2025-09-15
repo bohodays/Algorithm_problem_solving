be42a6f1eb5d57c71932cb3e84ba02c651dddfd4
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-09-15 22:28:35 +0900

    [level 2] Title: 방문 길이, Time: 0.77 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 1.29 ms
+메모리: 33.8 MB, 시간: 0.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 28일 14:27:47
+2025년 09월 15일 22:28:32
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,25 +1,34 @@
 function solution(dirs) {
     let answer = 0;
     
-    const d = {
-        U: [-1, 0], D: [1, 0], R: [0, 1], L: [0, -1]
-    }
-    const visited  = {};    
+    const direction = {
+        U: [-1, 0],
+        D: [1, 0],
+        L: [0, -1],
+        R: [0, 1]
+    };
+    const visited = {};
     
-    let [x, y] = [0, 0];
+    let x = 0, y = 0;
     for (const dir of dirs) {
-        const [nx, ny] = [x + d[dir][0], y + d[dir][1]];
+        const [dx, dy] = [direction[dir][0], direction[dir][1]];
+        const nx = x + dx, ny = y + dy;
         
+        // 범위 유효성 체크
         if (-5 <= nx && nx <= 5 && -5 <= ny && ny <= 5) {
-            if (!visited["" + x + y + nx + ny] || !visited["" + nx + ny + x + y]) {
+            // 방문 체크
+            if (!visited["" + x + y + nx + ny] && !visited["" + nx + ny + x + y]) {
                 visited["" + x + y + nx + ny] = true;
                 visited["" + nx + ny + x + y] = true;
-                answer += 1;
+                answer++;
             }
             x = nx;
             y = ny;
         }
+        
+        
     }
+    
     
     return answer;
 }
