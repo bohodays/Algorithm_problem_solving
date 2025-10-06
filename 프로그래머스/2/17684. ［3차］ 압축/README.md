1b5a76949d28520a5af77d336f39a1593e9269f9
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-10-06 14:31:34 +0900

    [level 2] Title: [3차] 압축, Time: 0.66 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.74 ms
+메모리: 33.6 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 27일 12:10:54
+2025년 10월 06일 14:31:31
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,55 +1,55 @@
 function solution(msg) {
     const answer = [];
-    let index = 27;
-    const dictionary = {
-        A: 1,
-        B: 2,
-        C: 3,
-        D: 4,
-        E: 5,
-        F: 6,
-        G: 7,
-        H: 8,
-        I: 9,
-        J: 10,
-        K: 11,
-        L: 12,
-        M: 13,
-        N: 14,
-        O: 15,
-        P: 16,
-        Q: 17,
-        R: 18,
-        S: 19,
-        T: 20,
-        U: 21,
-        V: 22,
-        W: 23,
-        X: 24,
-        Y: 25,
-        Z: 26,
-    };
+    let currentIndex = 27;
+    const indexDictionary = {
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        E: "5",
+        F: "6",
+        G: "7",
+        H: "8",
+        I: "9",
+        J: "10",
+        K: "11",
+        L: "12",
+        M: "13",
+        N: "14",
+        O: "15",
+        P: "16",
+        Q: "17",
+        R: "18",
+        S: "19",
+        T: "20",
+        U: "21",
+        V: "22",
+        W: "23",
+        X: "24",
+        Y: "25",
+        Z: "26"
+    }
     
-    while (msg) {
+    msg = msg.split("").reverse();
+    while (msg.length) {
+        let currentItem = msg.pop();
+        let result = indexDictionary[currentItem];
         
-        let newWord = "";
-        let w = "";
-        for (let i = 0; i < msg.length; i++) {
-            const targetWord = msg.slice(0, i + 1);
+        while (msg.length) {
+            currentItem += msg[msg.length - 1];
             
-            if (dictionary[targetWord]) {
-                w = targetWord;
+            if (indexDictionary[currentItem]) {
+                result = indexDictionary[currentItem];
+                msg.pop();
             } else {
-                newWord = targetWord;
+                indexDictionary[currentItem] = currentIndex++;
                 break;
             }
         }
         
-        answer.push(dictionary[w]);
-        dictionary[newWord] = index++;
-        msg = msg.replace(w, "")
+        answer.push(Number(result));
+        
     }
-
     
     return answer;
 }
