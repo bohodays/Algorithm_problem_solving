4814b2a994f751fa7858488a6a1d86a21764ac91
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-03 01:39:04 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 6.99 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
 
 ### 성능 요약
 
-메모리: 38.8 MB, 시간: 10.09 ms
+메모리: 38.3 MB, 시간: 6.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 01:19:15
+2025년 11월 03일 01:39:01
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,47 +1,37 @@
-function fileFilter(file) {
-    let HEAD = "";
-    let HEAD_flag = false;
-    let NUMBER = "";
-    let NUMBER_flag = false;
-    let TAIL = "";
-
-    for (let i = 0; i < file.length; i++) {
-        if (!HEAD_flag) {
-            if (/\d/.test(file[i])) {
-                HEAD_flag = true;
-            } else {
-                HEAD += file[i];
-            }
-        }
-
-        if (HEAD_flag && !NUMBER_flag) {
-            if (/\d/.test(file[i])) {
-                NUMBER += file[i];
-            } else {
-                NUMBER_flag = true;
-            }
-        }
-
-        if (HEAD_flag && NUMBER_flag) {
-            TAIL += file[i];
-        }
-    }
-
-    return [HEAD, NUMBER, TAIL];
-}
-
 function solution(files) {
-  let result = files.map(file => fileFilter(file));
+    const answer = [];
+    
+    let splitedFiles = files.map((file) => {
+        let HEAD_FLAG = true;
+        let NUMBER_FLAG = true;
+        let HEAD = "";
+        let NUMBER = "";
+        let TAIL = "";
+        
+        for (const s of file) {
+            if (isNaN(s) || s === " ") {
+                if (HEAD_FLAG) HEAD += s;
+                else {
+                    NUMBER_FLAG = false;
+                    TAIL += s;
+                }
+            } else {
+                HEAD_FLAG = false;
+                if (NUMBER_FLAG) NUMBER += s;
+                else {
+                    TAIL += s;
+                }
+            }
+        }
+        
+        return [HEAD.toLowerCase(), Number(NUMBER), TAIL.toLowerCase(), file];
+    })
 
-    // 정렬: HEAD는 대소문자 구분 없이 비교, NUMBER는 정수로 비교
-    result.sort((a, b) => {
-        const headA = a[0].toUpperCase();
-        const headB = b[0].toUpperCase();
-        if (headA < headB) return -1;
-        if (headA > headB) return 1;
-        return parseInt(a[1], 10) - parseInt(b[1], 10);
-    });
+    splitedFiles = splitedFiles.sort((a, b) => {
+        if (a[0] < b[0]) return -1; // a가 앞으로 (오름차순)
+        if (a[0] > b[0]) return 1; // b가 앞으로 (내림차순)
+        return a[1] - b[1];
+    }).map((file) => file[3])
 
-    // 원래 형식으로 합쳐 반환
-    return result.map(file => file.join(""));
+    return splitedFiles;
 }
