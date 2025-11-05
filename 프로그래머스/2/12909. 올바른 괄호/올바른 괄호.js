24e1ce2035f5fb3cc8c50f4b0652fcd906ba11f1
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-05 21:58:21 +0900

    [level 2] Title: 올바른 괄호, Time: 30.86 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,8 +1,8 @@
 function solution(s){
-    const stack = [];
     
-    for (item of s) {
-        if (!stack.length) stack.push(item);
+    const stack = [];
+    for (const item of s) {
+        if (stack.length === 0) stack.push(item);
         else {
             if (stack[stack.length - 1] === "(" && item === ")") stack.pop();
             else stack.push(item);
