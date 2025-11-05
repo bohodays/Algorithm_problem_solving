d7987e37f72bfb8dd7168ac9fc664f54dd6451a8
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-05 21:43:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.43 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,13 @@
 function solution(arr)
 {
     const answer = [];
-    
-    arr.forEach((item, i) => {
-        if (i === 0) answer.push(item);
-        else if (item !== answer[answer.length - 1]) answer.push(item);
-    })
 
+    arr.forEach((item) => {
+        if (answer.length === 0) answer.push(item);
+        else {
+            if (answer[answer.length - 1] !== item) answer.push(item);
+        }
+    })
+    
     return answer;
 }
