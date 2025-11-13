3842ab96976848b522c8c76dbe603f1027f2f072
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-13 21:33:14 +0900

    [level 1] Title: 모의고사, Time: 3.02 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.1 MB, 시간: 3.52 ms
+메모리: 35.9 MB, 시간: 3.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 27일 19:09:33
+2025년 11월 13일 21:33:12
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,15 +1,20 @@
 function solution(answers) {
-    const answer = [];
+    const result = [0, 0, 0];
     let answer1 = [1, 2, 3, 4, 5];
     let answer2 = [2, 1, 2, 3, 2, 4, 2, 5];
     let answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    const result1 = answers.map((item, index) => item === answer1[index % answer1.length] ? 1 : 0).reduce((sum, num) => sum + num);
-    const result2 = answers.map((item, index) => item === answer2[index % answer2.length] ? 1 : 0).reduce((sum, num) => sum + num);
-    const result3 = answers.map((item, index) => item === answer3[index % answer3.length] ? 1 : 0).reduce((sum, num) => sum + num);
-    const maxValue = Math.max(result1, result2, result3);
-    if (result1 === maxValue) answer.push(1);
-    if (result2 === maxValue) answer.push(2);
-    if (result3 === maxValue) answer.push(3);
+    answers.forEach((answer, index) => {
+        if (answer1[index % answer1.length] === answer) result[0] += 1;
+        if (answer2[index % answer2.length] === answer) result[1] += 1;
+        if (answer3[index % answer3.length] === answer) result[2] += 1;
+    })
+    
+    const maxAnswer = Math.max(...result);
+    const answer = [];
+    result.forEach((item, i) => {
+        if (item === maxAnswer) answer.push(i + 1) 
+    })
+
     return answer;
 }
