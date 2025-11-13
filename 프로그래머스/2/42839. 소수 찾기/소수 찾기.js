f8a517e8349ef1cfd001753c30b9956fc2d39b48
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-13 21:45:17 +0900

    [level 2] Title: 소수 찾기, Time: 5.69 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,37 +1,37 @@
-const isPrime = (number) => {
-    if (number === 0 || number === 1) return false;
+const isPrime = (n) => {
+    if (n <= 1) return false;
     
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
+    for (let i = 2; i <= parseInt(n**(1/2)); i++) {
+        if (n % i === 0) return false;
     }
+    
     return true;
 }
 
 function solution(numbers) {
     let answer = 0;
     
-    const result = new Set();
-    const visited = Array(numbers.length - 1).fill(false);
-    const dfs = (number) => {
+    const visited = Array(numbers.length).fill(false);
+    const numberSet = new Set();
+    
+    const dfs = (number, count) => {
+        
+        if (number.length) numberSet.add(Number(number));
         
         for (let i = 0; i < numbers.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                
-                const nextNumber = number + numbers[i];
-                if (isPrime(Number(nextNumber))) {
-                    result.add(Number(nextNumber));
-                }
-                dfs(nextNumber);
-                
+                dfs(number + numbers[i], count++);
                 visited[i] = false;
             }
-            
         }
     }
     
-    dfs("");
-    return result.size;
+    dfs("", 0);
+    
+    [...numberSet].forEach((number) => {
+        if (isPrime(number)) answer++;
+    })
+    
+    return answer;
 }
