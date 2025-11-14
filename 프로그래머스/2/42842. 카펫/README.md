065fdb83bf002e3bfcdaca765564dd60158f22ba
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-14 20:24:26 +0900

    [level 2] Title: 카펫, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.37 ms
+메모리: 33.5 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 21:27:40
+2025년 11월 14일 20:24:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,17 @@
 function solution(brown, yellow) {
-    
-    for (let i = 1; i <= Math.sqrt(yellow); i++) {
-        const width = yellow / i;
-        const height = i;
-        
-        if (brown === width * 2 + height * 2 + 4) {
-            return [width + 2, height + 2];
+    const answer = [];
+    const total = brown + yellow;
+    let width = 0, height = 0;
+    // brown + yellow의 약수구하기
+    for (let i = 1; i <= parseInt((total)**(1/2)); i++) {
+        if (total % i === 0) {
+            const j = total / i;
+            if (i + j === (brown + 4) / 2) {
+                width = j;
+                height = i;
+            }
         }
     }
+    
+    return [width, height];
 }
