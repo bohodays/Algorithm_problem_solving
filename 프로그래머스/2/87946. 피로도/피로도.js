1985fce182ac39988e9b5864ef0307aaae07194c
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-16 11:39:21 +0900

    [level 2] Title: 피로도, Time: 8.37 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -2,22 +2,24 @@ function solution(k, dungeons) {
     let answer = 0;
     const n = dungeons.length;
     const visited = Array(n).fill(false);
-
-    const dfs = (count, k) => {
+    
+    const dfs = (k, count) => {
+        answer = Math.max(answer, count);
         
         for (let i = 0; i < n; i++) {
             const [need, cost] = dungeons[i];
-            if (!visited[i] && k >= need) {
-                visited[i] = true;
-                dfs(count + 1, k - cost);
-                visited[i] = false;
+            if (k >= need) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    dfs(k - cost, count + 1);
+                    visited[i] = false;
+                }
             }
         }
         
-        answer = Math.max(answer, count);
     }
     
-    dfs(0, k);
+    dfs(k, 0)
     
     return answer;
 }
