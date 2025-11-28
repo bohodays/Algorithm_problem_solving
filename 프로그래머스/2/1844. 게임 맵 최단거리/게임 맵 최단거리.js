4d75342c9648f6c77eec7f885b53d8c8eed541db
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-11-29 00:57:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.59 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -2,41 +2,30 @@ function solution(maps) {
     let answer = 0;
     const n = maps.length;
     const m = maps[0].length;
-    
     const visited = Array(n).fill().map((_) => Array(m).fill(0));
+    const d = [[-1, 0], [1, 0], [0, -1], [0, 1]]; // 상하좌우
     
-    const bfs = (n, m, visited) => {
+    // 시작지점 세팅
+    visited[0][0] = 1;
+    const queue = [[0, 0]];
+    
+    while (queue.length) {
+        const [x, y] = queue.shift();
         
-        // 상하좌우
-        const d = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-        
-        // 시작지점 추가
-        visited[0][0] = 1;
-        const queue = [[0, 0]];
-        
-        // 탐색
-        while (queue.length) {
-            const [x, y] = queue.shift();
-            
-            // 4방향 확인
-            for (const [dx, dy] of d) {
-                const nx = x + dx;
-                const ny = y + dy;
-                // 범위 확인
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    // 방문 여부 + 벽이 없는지 확인
-                    if (maps[nx][ny] && !visited[nx][ny]) {
-                        visited[nx][ny] = visited[x][y] + 1;
-                        queue.push([nx, ny])
-                    }
+        // 4방향 탐색
+        for (const [dx, dy] of d) {
+            const nx = x + dx;
+            const ny = y + dy;
+            // 범위 유효성 체크
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                // 방문 및 벽 여부 유효성 체크
+                if (maps[nx][ny] && !visited[nx][ny]) {
+                    visited[nx][ny] = visited[x][y] + 1;
+                    queue.push([nx, ny]);
                 }
             }
         }
-        
-        return visited; 
-    };
+    }
     
-    const result = bfs(n, m, visited)
-    
-    return result[n - 1][m - 1] || -1;
+    return visited[n - 1][m - 1] || -1;
 }
