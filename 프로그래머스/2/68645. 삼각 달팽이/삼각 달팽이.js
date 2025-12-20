81461db779fae16b5e25fcaf6d08eb349f8379b5
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-12-21 02:01:42 +0900

    [level 2] Title: 삼각 달팽이, Time: 93.03 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,50 +1,44 @@
 function solution(n) {
-    if (n === 1) return [1];
-    if (n === 2) return [1, 2, 3];
-    
     const answer = [];
     const table = Array(n).fill().map((_) => Array(n).fill(0));
-    
-    let limit = 0;
-    for (let i = 0; i <= n; i++) {
-        limit += i;
-    }
-    
-    // 방향 설정
-    const d =  [[1, -1], [0, 1], [-1, 0]];
-    let direction = 0;
-    let count = 1;
-    let [x, y] = [0, n - 1]
+    const target = n**2 / 2 + n / 2;
+
+    // 직각삼각형 방향 회전 설정
+    const d = [[1, -1], [0, 1], [-1, 0]];
     
     // 시작지점 설정
+    const queue = [[0, n - 1]];
     table[0][n - 1] = 1;
+    let index = 0;
     
-    while (true) {
-        const [nx, ny] = [x + d[direction][0], y + d[direction][1]];
+    while (queue.length) {
+        const [x, y] = queue.pop();
         
+        if (table[x][y] === target) break;
+        
+        const [nx, ny] = [x + d[index][0], y + d[index][1]];
+        
+        // 범위 체크
         if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-            if (table[nx][ny] === 0) {
+            // 방문 여부 체크
+            if (!table[nx][ny]) {
                 table[nx][ny] = table[x][y] + 1;
-                
-                if (table[nx][ny] === limit) break;
-                
-                x = nx;
-                y = ny;
+                queue.push([nx, ny])
             } else {
-                direction = (direction + 1) % 3;
+                index = (index + 1) % 3;
+                queue.push([x, y]);
             }
         } else {
-            direction = (direction + 1) % 3;
+            index = (index + 1) % 3;
+            queue.push([x, y]);
         }
     }
-    
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (table[i][j] !== 0) {
-                answer.push(table[i][j])
-            }            
-        }
-    }
+
+    table.forEach((row) => {
+        row.forEach((num) => {
+            if (num) answer.push(num)
+        })
+    })
     
     return answer;
 }
