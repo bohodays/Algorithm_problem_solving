fe502cda88d37ad56037bf0fb789d95a43535cec
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2025-12-28 21:19:36 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 30일 17:27:10
+2025년 12월 28일 21:19:33
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -4,12 +4,12 @@ function solution(storey) {
     while (storey) {
         const dividedValue = storey % 10;
         
-        if (dividedValue < 10 - dividedValue) {
-            answer += dividedValue;
-            storey = parseInt(storey / 10);
-        } else if (dividedValue > 10 - dividedValue) {
+        if (dividedValue > 5) {
             answer += 10 - dividedValue;
             storey = parseInt(storey / 10) + 1;
+        } else if (dividedValue < 5) {
+            answer += dividedValue;
+            storey = parseInt(storey / 10);
         } else {
             const checkValue = parseInt(storey / 10) % 10;
             if (checkValue >= 5) {
