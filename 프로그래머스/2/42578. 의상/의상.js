f4dedad4afa23a2788353a09e94a97a8ff26c193
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-05-17 23:52:41 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,15 @@
 function solution(clothes) {
     let answer = 1;
-    
     const clothesRef = {};
     clothes.forEach(([cloth, type]) => {
-        if (clothesRef[type]) clothesRef[type].push(cloth);
-        else clothesRef[type] = [cloth];
-    })
+        clothesRef[type] = (clothesRef[type] || 0) + 1;
+    });
     
-    for (const value of Object.values(clothesRef)) {
-        answer *= value.length + 1;
+    // 각 유형별 개수 + 1 곱하기
+    for (const type in clothesRef) {
+        answer *= clothesRef[type] + 1;
     }
     
+    // 모두 선택하지 않는 케이스 제외
     return answer - 1;
 }
