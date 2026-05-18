35be5ea3cf84fd1b805a77700d98282f12e318cd
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-05-18 20:53:22 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -2,8 +2,8 @@ function solution(array, commands) {
     const answer = [];
     
     commands.forEach(([i, j, k]) => {
-        const result = array.slice(i - 1, j).sort((a, b) => a - b);
-        answer.push(result[k - 1]);
+        const target = array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+        answer.push(target);
     })
     
     return answer;
