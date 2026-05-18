813530051876cea84c591199ba7b9d430f8a7a7b
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-05-18 22:15:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 15.81 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,23 +1,20 @@
 function solution(bridge_length, weight, truck_weights) {
     let time = 0;
-    const bridge = Array(bridge_length).fill(0); // 다리 위 상태(앞이 출구)
+    const bridge = Array(bridge_length).fill(0);
     let currentWeight = 0;
     
     while (truck_weights.length) {
         time++;
         currentWeight -= bridge.shift();
         
-        if (truck_weights[0] + currentWeight <= weight) {
-            const truck = truck_weights.shift();
-            bridge.push(truck);
-            currentWeight += truck;
+        if (currentWeight + truck_weights[0] <= weight) {
+            const shiftedTruck = truck_weights.shift();
+            currentWeight += shiftedTruck;
+            bridge.push(shiftedTruck);
         } else {
-            bridge.push(0);
+            bridge.push(0)
         }
-        
     }
     
-    time += bridge.length;
-    
-    return time;
+    return time + bridge_length;
 }
