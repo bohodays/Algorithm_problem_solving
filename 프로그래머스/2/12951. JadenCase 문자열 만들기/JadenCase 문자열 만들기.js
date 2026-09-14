f274d872eb5c9c69371436f2700836db1bef1c00
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-14 23:48:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.28 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,21 +1,19 @@
 function solution(s) {
     let answer = '';
-    
-    let temp = ""
-    for (const word of s) {
-        if (word !== " ") {
-            temp += word;
-        } else {
-            if (temp.length > 0) {
-                answer += temp[0].toUpperCase() + temp.slice(1).toLowerCase();
-                temp = "";
+    let result = '';
+    for (const item of s) {
+        if (item !== ' ') result += item;
+        else {
+            if (result.length) {
+                answer += result[0].toUpperCase() + result.slice(1).toLowerCase();
+                result = '';
             }
-            answer += word;
+            answer += item;
         }
     }
     
-    if (temp.length > 0) {
-        answer += temp[0].toUpperCase() + temp.slice(1).toLowerCase();
+    if (result.length) {
+        answer += result[0].toUpperCase() + result.slice(1).toLowerCase();
     }
     
     return answer;
