d0cd4e47d46d5c244bf7fbc5d39fc1f7ad04f0f2
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-14 23:09:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,6 @@
 function solution(s) {
-    const result = s.split(" ").map((num) => +num).sort((a, b) => a - b);
-    return `${result[0]} ${result[result.length - 1]}`;
+    const nums = s.split(" ").map((n) => Number(n));
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+    return `${min} ${max}`;
 }
