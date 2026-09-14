2fe4ba61f988e267c35857c4195d3867147bebfd
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-14 23:06:52 +0900

    [level 2] Title: 올바른 괄호, Time: 19.05 ms, Memory: 50.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,12 @@
 function solution(s){
     const stack = [];
-
     for (const item of s) {
         if (!stack.length) stack.push(item);
         else {
-            if (stack[stack.length - 1] === '(' && item === ')') stack.pop();
+            if (stack[stack.length - 1] === '(' && item === ')') stack.pop()
             else stack.push(item);
         }
     }
-    
-    return stack.length ? false : true;
+
+    return stack.length === 0;
 }
