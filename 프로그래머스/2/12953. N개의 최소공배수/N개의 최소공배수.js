e9b9091d8977e121b604905c908adad33deb1d57
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-15 21:03:23 +0900

    [level 2] Title: N개의 최소공배수, Time: 11.78 ms, Memory: 48.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,18 +1,26 @@
-// 최대공약수 구하기
-const gcd = (a, b) => {
-    while (b !== 0) {
-        [a, b] = [b, a % b];
-    }
-    return a;
-};
-
-// 최소공배수 구하기
-const lcm = (a, b) => {
-    return (a * b) / gcd(a, b);
-}
-
-
 function solution(arr) {
-    const answer = arr.reduce((result, num) => lcm(result, num), 1);
+    let answer = 0;
+    const maxValue = Math.max(...arr);
+    const n = arr.length;
+    
+    let curr = 1;
+    while (true) {
+        let flag = false;
+        const check = maxValue * curr;
+        for (let i = 0; i < n; i++) {
+            if (check % arr[i] !== 0) {
+                flag = true;
+                break;
+            }
+        }
+        
+        if (flag) {
+            curr++;
+        } else {
+            answer = check;
+            break;
+        }
+    }
+    
     return answer;
 }
