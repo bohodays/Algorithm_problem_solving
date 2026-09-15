7ceb3277880d089f201b1e0321b22cee23477501
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-15 20:50:07 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.14 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -3,13 +3,11 @@ function solution(n)
     let ans = 0;
     
     while (n) {
-        // 홀수이면
-        if (n % 2 === 1) {
+        if (n % 2 === 0) n /= 2;
+        else {
             n--;
             ans++;
         }
-        // 짝수이면
-        else n /= 2;
     }
 
     return ans;
