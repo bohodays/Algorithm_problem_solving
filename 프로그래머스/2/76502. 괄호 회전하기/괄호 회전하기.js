844494bc3e6cabf5c8f783eda165556a0e7664ce
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-15 22:18:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 109.92 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,26 +1,27 @@
-const onCheckItem = (s) => {
-    const stack = [];
-    for (const item of s) {
-        if (!stack.length) stack.push(item);
-        else {
-            if (item === "]" && stack[stack.length - 1] === "[") stack.pop();
-            else if (item === ")" && stack[stack.length - 1] === "(") stack.pop();
-            else if (item === "}" && stack[stack.length - 1] === "{") stack.pop();
-            else stack.push(item);
-        }
-    }
-    return stack.length === 0 ? true : false;
-}
-
 function solution(s) {
     let answer = 0;
     const n = s.length;
-    
-    s += s;
-    for (let i = 0; i < n; i++) {
-        if (onCheckItem(s.slice(i, i + n))) answer++;
+    s = s + s;
+    const ref = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
     }
     
+    for (let i = 0; i < n; i++) {
+        const slicedS = s.slice(i, i + n);
+        
+        const stack = [];
+        for (const item of slicedS) {
+            if (!stack.length) stack.push(item);
+            else {
+                if (ref[stack[stack.length - 1]] === item) stack.pop();
+                else stack.push(item);
+            }
+        }
+        
+        if (!stack.length) answer++
+    }
     
     return answer;
 }
