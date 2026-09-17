1f9ab693355230a503eca90a734cce03e83d54a9
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-17 19:31:35 +0900

    [level 2] Title: H-Index, Time: 141.95 ms, Memory: 50.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,17 +1,13 @@
 function solution(citations) {
     let answer = 0;
     
-    for (let h = 0; h <= Math.max(...citations); h++) {
-        const stack1 = [];
-        const stack2 = [];
-        
-        citations.forEach((citation) => {
-            if (citation >= h) stack1.push(citation);
-            else stack2.push(citation);
-        });
-        
-        if (stack1.length >= h && stack2.length <= h) answer = Math.max(answer, h);
-    };
+    for (let i = 0; i <= Math.max(...citations); i++) {
+        if (citations.filter((a) => a >= i).length >= i) {
+            if (citations.filter((b) => b < i).length <= i) {
+                answer = i;
+            }
+        }
+    }
     
     return answer;
 }
