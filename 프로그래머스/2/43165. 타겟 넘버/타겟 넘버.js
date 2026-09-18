a9fd2e494a6cd44009e2f572b78f0dd7a2e65f36
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 23:09:56 +0900

    [level 2] Title: 타겟 넘버, Time: 22.43 ms, Memory: 48 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,19 @@
 function solution(numbers, target) {
     let answer = 0;
     
-    const dfs = (num, count) => {
-        // 종료조건
-        if (count === numbers.length) {
-            if (num === target) answer++;
+    const dfs = (curr, result) => {
+        // 종료 조건
+        if (curr === numbers.length) {
+            if (result === target) answer++;
             
             return;
         }
         
-        dfs(num + numbers[count], count + 1);
-        dfs(num - numbers[count], count + 1);
+        dfs(curr + 1, result + numbers[curr]);
+        dfs(curr + 1, result - numbers[curr]);
     }
     
-    dfs(0, 0);
+    dfs(0, 0)
     
     return answer;
 }
