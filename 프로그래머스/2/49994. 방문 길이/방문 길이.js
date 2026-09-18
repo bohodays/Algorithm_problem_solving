0d83b90b10b27dd194a7feaf8ae328755ac11155
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 23:51:48 +0900

    [level 2] Title: 방문 길이, Time: 0.33 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,34 +1,34 @@
 function solution(dirs) {
     let answer = 0;
-    
-    const direction = {
-        U: [-1, 0],
-        D: [1, 0],
-        L: [0, -1],
-        R: [0, 1]
+    const n = 5;
+    const d = {
+        'U': [-1, 0],
+        'D': [1, 0],
+        'L': [0, -1],
+        'R': [0, 1]
     };
-    const visited = {};
+    const ref = new Map();
     
-    let x = 0, y = 0;
-    for (const dir of dirs) {
-        const [dx, dy] = [direction[dir][0], direction[dir][1]];
-        const nx = x + dx, ny = y + dy;
+    let x = 0;
+    let y = 0;
+    
+    dirs.split("").forEach((dir) => {
+        const nx = x + d[dir][0];
+        const ny = y + d[dir][1];
         
-        // 범위 유효성 체크
+        // 범위 확인
         if (-5 <= nx && nx <= 5 && -5 <= ny && ny <= 5) {
-            // 방문 체크
-            if (!visited["" + x + y + nx + ny] && !visited["" + nx + ny + x + y]) {
-                visited["" + x + y + nx + ny] = true;
-                visited["" + nx + ny + x + y] = true;
+            const path1 = `${x}${y}|${nx}${ny}`;
+            const path2 = `${nx}${ny}|${x}${y}`;
+            if (!ref.has(path1) && !ref.has(path2)) {
+                ref.set(path1, true);
+                ref.set(path2, true);
                 answer++;
             }
             x = nx;
             y = ny;
         }
-        
-        
-    }
-    
+    })
     
     return answer;
 }
