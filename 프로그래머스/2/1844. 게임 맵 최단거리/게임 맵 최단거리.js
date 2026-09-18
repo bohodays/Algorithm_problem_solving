ae19b84dbffe07d2dd25798b8df87c7daf353029
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 22:03:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.48 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,31 +1,30 @@
 function solution(maps) {
+    let answer = 0;
     const n = maps.length;
     const m = maps[0].length;
-    const table = Array(n).fill().map((_) => Array(m).fill(0));
+    const visited = Array(n).fill().map((_) => Array(m).fill(0));
+    const d = [[-1, 0], [1, 0], [0, -1], [0, 1]]; // 상하좌우
     
-    // 방향 (상 하 좌 우)
-    const d = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    
-    // 시작지점 설정
-    table[0][0] = 1;
+    // 시작지점 표시
     const queue = [[0, 0]];
+    visited[0][0] = 1;
     
     while (queue.length) {
         const [x, y] = queue.shift();
         
-        // 4방향 탐색
         for (let i = 0; i < 4; i++) {
-            const [nx, ny] = [x + d[i][0], y + d[i][1]];
-            // 범위 확인
+            const nx = x + d[i][0];
+            const ny = y + d[i][1];
+            // 유효 범위 확인
             if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                // 방문여부 & 벽 확인
-                if (!table[nx][ny] && maps[nx][ny] === 1) {
-                    table[nx][ny] = table[x][y] + 1;
-                    queue.push([nx, ny]);
+                // 방문 및 벽 여부 확인
+                if (!visited[nx][ny] && maps[nx][ny] === 1) {
+                    visited[nx][ny] = visited[x][y] + 1;
+                    queue.push([nx, ny])
                 }
-            }
+            }            
         }
     }
     
-    return table[n - 1][m - 1] || -1;
+    return visited[n - 1][m - 1] || -1;
 }
