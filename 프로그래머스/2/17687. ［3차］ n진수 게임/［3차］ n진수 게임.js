2ca2080857ce8b9e69f2d6b4559c6a0c8efb384c
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-19 00:25:20 +0900

    [level 2] Title: [3차] n진수 게임, Time: 37.78 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,9 +1,10 @@
 function solution(n, t, m, p) {
     let answer = '';
-    const totalNumber = Array(t * m).fill().map((_, i) => i.toString(n).toUpperCase()).join("");
+    const totalRef = Array(t * m).fill().map((_, i) => i.toString(n).toUpperCase()).join("");
     
-    for (let i = p - 1; i < t * m; i += m) {
-        answer += totalNumber[i];
+    for (let i = p - 1; i < totalRef.length; i += m) {
+        answer += totalRef[i];
+        if (answer.length === t) break;
     }
     
     return answer;
