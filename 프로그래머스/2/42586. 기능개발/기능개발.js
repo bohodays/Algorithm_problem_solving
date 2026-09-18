04db8cddc622773de515a08bf6169107b455bdf8
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 20:47:40 +0900

    [level 2] Title: 기능개발, Time: 0.45 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,21 +1,21 @@
 function solution(progresses, speeds) {
-    const answer = [];
+    let answer = [];
+    const n = progresses.length;
     
-    let currentCount = 0;
-    const limitCount = progresses.length;
-    while (currentCount < limitCount) {
+    let i = 0;
+    while (i < n) {
+        // 작업 진행하기
+        progresses.forEach((item, index) => progresses[index] += speeds[index]);
         
-        for (let i = currentCount; i < limitCount; i++) {
-            progresses[i] += speeds[i];
+        // 배포할 작업 계산하기
+        let count = 0;
+        while (progresses[i] >= 100) {
+            count++;
+            i++;
         }
         
-        let result = 0;
-        while (progresses[currentCount] >= 100) {
-            currentCount++;
-            result++;
-        }
-        
-        if (result) answer.push(result);
+        // 배포할 작업 있으면 반영하기
+        if (count) answer.push(count);
     }
     
     return answer;
