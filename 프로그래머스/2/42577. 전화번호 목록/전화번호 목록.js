deebfb5572822c1d8a8dd5834168ab743fc76643
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 20:52:35 +0900

    [level 2] Title: 전화번호 목록, Time: 105.33 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,17 @@
 function solution(phone_book) {
+    let answer = true;
+    
     // 사전순 정렬
-    phone_book = phone_book.sort();
+    phone_book.sort();
     
     for (let i = 0; i < phone_book.length - 1; i++) {
         const [currentItem, nextItem] = [phone_book[i], phone_book[i + 1]];
-        if (currentItem === nextItem.slice(0, currentItem.length)) return false;
+        
+        if (nextItem.startsWith(currentItem)) {
+            answer = false;
+            break;
+        }
     }
     
-    return true;
+    return answer;
 }
