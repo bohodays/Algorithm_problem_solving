592affebae8cb1f6883dd9f661cf3451233ee884
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-18 21:55:12 +0900

    [level 2] Title: [1차] 캐시, Time: 22.65 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 40.8 MB, 시간: 46.42 ms
+메모리: 54.7 MB, 시간: 22.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 06일 22:35:45
+2026년 09월 18일 21:55:11
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -2,36 +2,30 @@ function solution(cacheSize, cities) {
     let answer = 0;
     const CACHE_HIT = 1;
     const CACHE_MISS = 5;
-    const cache = [];
     
     // 캐시 사이즈가 0이면 캐시를 할 수 없으니 크기 * MISS 반환
     if (cacheSize === 0) return cities.length * CACHE_MISS;
     
+    const test = [1, 2, 3, 4];
+    
+    const cache = [];
     cities.forEach((city) => {
-        // 캐싱되어있는지 확인
         city = city.toUpperCase();
-        
-        const targetIndex = cache.indexOf(city);
-        
-        // 캐싱되어있는 경우
-        if (targetIndex !== -1) {
-            cache.splice(targetIndex, 1);
+        const index = cache.findIndex((i) => i === city);
+        // cache hit
+        if (index !== -1) {
+            cache.splice(index, 1);
             answer += CACHE_HIT;
-        } 
-        // 캐싱되어있지 않은 경우
+        }
+        // cache miss
         else {
             answer += CACHE_MISS;
-            
-            // 캐싱공간이 full인 경우
-            if (cache.length >= cacheSize) {
-                cache.shift();
-            }
+            // 캐시크기가 full이 아닌 경우
+            if (cache.length === cacheSize) cache.shift();
         }
         
-                    
         cache.push(city);
     })
-    
     
     return answer;
 }
