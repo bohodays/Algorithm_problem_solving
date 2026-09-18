592a0ea64d13077eb00f471a96da29d0185786a6
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-19 01:13:18 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.80 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,38 +1,54 @@
-// 문자열을 입력받아 두글자씩 끊어서 집합을 만드는 함수
-// 특수문자는 제외하며, 집합의 결과는 객체 형태로 반환함.
-const makeObject = (str) => {
+// 문자를 두 글자씩 끊어서 다중 집합으로 만드는 함수
+const convertStr = (str) => {
+    str = str.toUpperCase();
     const ref = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const result = {};
+    
+    const result = [];
     for (let i = 0; i < str.length - 1; i++) {
-        const [str1, str2] = [str[i].toUpperCase(), str[i + 1].toUpperCase()];
-        if (ref.includes(str1) && ref.includes(str2)) {
-            result[str1 + str2] = (result[str1 + str2] ?? 0) + 1;
+        // 공백 체크
+        if (str[i] && str[i + 1]) {
+            // 숫자 체크
+            if (isNaN(str[i]) && isNaN(str[i])) {
+                // 특수문자 체크
+                if (ref.includes(str[i]) && ref.includes(str[i + 1])) {
+                    result.push(str[i] + str[i + 1]);
+                }
+            }
         }
     }
+    
     return result;
 }
 
 function solution(str1, str2) {
     let answer = 0;
-    const str1ToObj = makeObject(str1);
-    const str2ToObj = makeObject(str2);
     
-    const intersection = Object.keys(str1ToObj).map((key) => {
-        if (Object.keys(str2ToObj).includes(key)) {
-            return Math.min(str1ToObj[key], str2ToObj[key]);
-        }
-        return 0;
-    }).reduce((sum, num) => sum + num, 0);
+    const convertStr1 = convertStr(str1);
+    const convertStr2 = convertStr(str2);
     
-    const unionArray = [];
-    for (const key of Object.keys(str1ToObj)) {
-        if (str2ToObj[key]) unionArray.push(Math.max(str1ToObj[key], str2ToObj[key]));
-        else unionArray.push(str1ToObj[key])
+    if (!convertStr1.length && !convertStr2.length) answer = 1;
+    else {
+        const totalArr = [...new Set(convertStr1.concat(convertStr2))];
+        const str1Map = new Map();
+        convertStr1.forEach((str) => str1Map.set(str, str1Map.has(str) ? str1Map.get(str) + 1 : 1));
+        const str2Map = new Map();
+        convertStr2.forEach((str) => str2Map.set(str, str2Map.has(str) ? str2Map.get(str) + 1 : 1));
+        
+        // 교집합
+        let intersection = 0;
+        totalArr.forEach((item) => {
+            if (str1Map.has(item) && str2Map.has(item)) intersection += Math.min(str1Map.get(item), str2Map.get(item));
+        })
+        
+        let union = 0;
+        totalArr.forEach((item) => {
+            if (str1Map.has(item) && str2Map.has(item)) union += Math.max(str1Map.get(item), str2Map.get(item));
+            else if (str1Map.has(item)) union += str1Map.get(item);
+            else if (str2Map.has(item)) union += str2Map.get(item);
+        })
+        
+        answer = intersection / union
     }
-    for (const key of Object.keys(str2ToObj)) {
-        if (!str1ToObj[key]) unionArray.push(str2ToObj[key]);
-    }
-    const union = unionArray.reduce((sum, num) => sum + num, 0)
     
-    return parseInt((union ? intersection / union : 1) * 65536)
+    return parseInt(answer * 65536);
 }
