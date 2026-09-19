56c094f52b42344e9882333f9fd2b6fa81dbfb53
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-19 16:57:16 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,25 +1,10 @@
 function solution(skill, skill_trees) {
     let answer = 0;
-    const isCorrect = skill.length;
     
-    for (let i = 0; i < skill_trees.length; i++) {
-        const currentSkillTree = skill_trees[i];
-        let result = 0;
-        let flag = true;
-        
-        for (let j = 0; j < currentSkillTree.length; j++) {
-            if (skill.includes(currentSkillTree[j])) {
-                if (currentSkillTree[j] === skill[result]) {
-                    result++;
-                } else {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        
-        if (result === isCorrect || flag) answer++;
-    }
+    const convertSkillTrees = skill_trees.map((skills) => skills.split("").filter((item) => skill.includes(item)).join(""));
+    convertSkillTrees.forEach((skillTree) => {
+        if (skillTree === skill.slice(0, skillTree.length)) answer++;
+    })
     
     return answer;
 }
