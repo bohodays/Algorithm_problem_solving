29def123eb03ca20da3c1177c4b82119806b5dd9
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-19 11:11:08 +0900

    [level 2] Title: 주식가격, Time: 24.12 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -2,12 +2,13 @@ function solution(prices) {
     const answer = [];
     
     for (let i = 0; i < prices.length; i++) {
-        let time = 0;
+        const currPrice = prices[i];
+        let sec = 0;
         for (let j = i + 1; j < prices.length; j++) {
-            time++;
-            if (prices[i] > prices[j]) break;
+            sec++;
+            if (prices[j] < currPrice) break;
         }
-        answer.push(time);
+        answer.push(sec);
     }
     
     return answer;
