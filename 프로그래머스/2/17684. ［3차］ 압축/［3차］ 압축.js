2b253a575f812181251d94731f9e60509219adab
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-19 17:20:32 +0900

    [level 2] Title: [3차] 압축, Time: 0.64 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,53 +1,28 @@
 function solution(msg) {
+    const wordMap = new Map();
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").forEach((word, i) => wordMap.set(word, i + 1));
+    let id = 27;
     const answer = [];
-    let currentIndex = 27;
-    const indexDictionary = {
-        A: "1",
-        B: "2",
-        C: "3",
-        D: "4",
-        E: "5",
-        F: "6",
-        G: "7",
-        H: "8",
-        I: "9",
-        J: "10",
-        K: "11",
-        L: "12",
-        M: "13",
-        N: "14",
-        O: "15",
-        P: "16",
-        Q: "17",
-        R: "18",
-        S: "19",
-        T: "20",
-        U: "21",
-        V: "22",
-        W: "23",
-        X: "24",
-        Y: "25",
-        Z: "26"
-    }
     
-    msg = msg.split("").reverse();
-    while (msg.length) {
-        let currentItem = msg.pop();
-        let result = indexDictionary[currentItem];
+    let i = 0;
+    while (i < msg.length) {
+        let j = i + 1;
         
-        while (msg.length) {
-            currentItem += msg[msg.length - 1];
-            
-            if (indexDictionary[currentItem]) {
-                result = indexDictionary[currentItem];
-                msg.pop();
-            } else {
-                indexDictionary[currentItem] = currentIndex++;
-                break;
-            }
+        let curr = msg[i];
+        // 색인 확인
+        while (wordMap.has(curr + msg[j])) {
+            curr += msg[j];
+            j++;            
         }
         
-        answer.push(Number(result));
+        if (!wordMap.has(curr + msg[j])) {
+            // 색인 등록
+            wordMap.set(curr + msg[j], id++);
+            answer.push(wordMap.get(curr));
+            i++;
+        }
+        
+        i += (j - i);
         
     }
     
