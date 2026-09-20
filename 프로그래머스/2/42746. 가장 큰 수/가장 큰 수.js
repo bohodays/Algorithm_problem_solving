931381ad261fdc3c394ba14f22018c0219da8d03
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-20 11:00:20 +0900

    [level 2] Title: 가장 큰 수, Time: 138.21 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,4 @@
 function solution(numbers) {
-    
-    const answer = numbers.sort((a, b) => ('' + b + a) - ('' + a + b)).join("");
-
+    const answer = numbers.sort((a, b) => ('' + a + b) - ('' + b + a)).reverse().join("");
     return answer[0] === "0" ? "0" : answer;
 }
