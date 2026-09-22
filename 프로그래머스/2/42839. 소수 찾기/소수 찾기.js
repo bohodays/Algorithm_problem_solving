dd23c65574a899d7d57371d42573a32759b2862e
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-22 23:56:35 +0900

    [level 2] Title: 소수 찾기, Time: 11.16 ms, Memory: 49.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,37 +1,32 @@
-const isPrime = (n) => {
-    if (n <= 1) return false;
+const isPrime = (num) => {
+    if (num <= 1) return false;
     
-    for (let i = 2; i <= parseInt(n**(1/2)); i++) {
-        if (n % i === 0) return false;
+    for (let i = 2; i <= parseInt(num**(1/2)); i++) {
+        if (num % i === 0) return false;
     }
     
     return true;
 }
 
 function solution(numbers) {
-    let answer = 0;
-    
     const visited = Array(numbers.length).fill(false);
-    const numberSet = new Set();
+    const nums = new Set();
     
-    const dfs = (number, count) => {
-        
-        if (number.length) numberSet.add(Number(number));
+    const dfs = (num) => {
+        if (num) nums.add(Number(num));
         
         for (let i = 0; i < numbers.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(number + numbers[i], count++);
+                dfs(num + numbers[i]);
                 visited[i] = false;
             }
         }
-    }
+    };
     
-    dfs("", 0);
+    dfs("");
     
-    [...numberSet].forEach((number) => {
-        if (isPrime(number)) answer++;
-    })
+    const answer = [...nums].filter((num) => isPrime(num)).length;
     
     return answer;
 }
