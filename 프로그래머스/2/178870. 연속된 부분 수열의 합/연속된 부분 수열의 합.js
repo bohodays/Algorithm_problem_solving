ed2d7bd1fa73b1dd9eb37de985a01a13173db0d7
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-24 13:01:22 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 91.40 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,14 +1,13 @@
 function solution(sequence, k) {
     const answer = [];
-    
     let start = 0;
     let total = 0;
+    
     for (let i = 0; i < sequence.length; i++) {
         total += sequence[i];
         
         while (total > k) {
-            total -= sequence[start];
-            start++;
+            total -= sequence[start++];
         }
         
         if (total === k) {
@@ -17,11 +16,8 @@ function solution(sequence, k) {
     }
     
     answer.sort((a, b) => {
-        if (b[1] - b[0] === a[1] - a[0]) {
-            return a[0] - b[0];
-        } else {
-            return (a[1] - a[0]) - (b[1] - b[0]);
-        }
+        if ((a[1] - a[0]) !== (b[1] - b[0])) return (a[1] - a[0]) - (b[1] - b[0]);
+        return a[0] - b[0];
     })
     
     return answer[0];
