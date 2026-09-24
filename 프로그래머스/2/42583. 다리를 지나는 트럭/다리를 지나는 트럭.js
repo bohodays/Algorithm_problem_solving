27b269566175fca3af5c613372c739b0db2ef982
Jungwon Park <109454527+bohodays@users.noreply.github.com>
2026-09-24 20:59:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 2569.98 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,20 +1,19 @@
 function solution(bridge_length, weight, truck_weights) {
     let time = 0;
-    const bridge = Array(bridge_length).fill(0);
-    let currentWeight = 0;
+    const stack = Array(bridge_length).fill(0);
     
     while (truck_weights.length) {
         time++;
-        currentWeight -= bridge.shift();
+        stack.shift();
         
-        if (currentWeight + truck_weights[0] <= weight) {
-            const shiftedTruck = truck_weights.shift();
-            currentWeight += shiftedTruck;
-            bridge.push(shiftedTruck);
+        if (stack.filter((i) => i !== 0).length < bridge_length && stack.reduce((sum, num) => sum + num, 0) + truck_weights[0] <= weight) {
+            stack.push(truck_weights.shift());
         } else {
-            bridge.push(0)
+            stack.push(0)
         }
     }
     
-    return time + bridge_length;
+    const lastIndex = stack.findLastIndex((num) => num !== 0);
+    
+    return time + (lastIndex + 1);
 }
